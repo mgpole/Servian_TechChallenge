f151b15ea1f788111ea77639b65704702aa025f1
Michael Pole <michael.pole@aware.com.au>
2021-08-26 13:48:33 +1000

    add autoscaling for app service plan

--- terraform/main.tf
+++ terraform/main.tf
@@ -168,6 +168,57 @@ resource "azurerm_app_service" "app_service" {
 
 }
 
+resource "azurerm_monitor_autoscale_setting" "app_service_autoscale" {
+  name                = join("-", [var.ENVIRONMENT_NAME, var.PRODUCT_NAME, "web", "autoscale"])
+  resource_group_name = azurerm_resource_group.pri_rg.name
+  location            = var.pri_location
+  target_resource_id  = azurerm_app_service_plan.web_asp.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.web_asp.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 80
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.web_asp.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 20
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }  
+}
+
 resource "azurerm_app_service_virtual_network_swift_connection" "vnet_integration" {
    app_service_id = azurerm_app_service.app_service.id
    subnet_id      = azurerm_subnet.web_subnet.id
